a907bc46626d225a497b0891b41f919a2339c708
cgarnette <cgarnette18@gmail.com>
2018-08-20 20:24:18 -0400

    simple form

--- homeless/src/components/MainPage.jsx
+++ homeless/src/components/MainPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-class MainPage extends React.Component{
+export default class MainPage extends React.Component{
     constructor(props){
         super(props)
         this.state = {}

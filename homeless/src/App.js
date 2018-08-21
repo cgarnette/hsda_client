369e3c50767b0f249756cc40aef136821fb7d795
cgarnette <cgarnette18@gmail.com>
2018-08-20 20:20:47 -0400

    initial upload

---
+++ homeless/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainPage from '../src/components/MainPage';
+
+class App extends Component {
+  render() {
+    return (
+      <MainPage/>
+    );
+  }
+}
+
+export default App;
---
+++ homeless/src/components/MainPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class MainPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    onSubmit = event =>{
+        return;
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
